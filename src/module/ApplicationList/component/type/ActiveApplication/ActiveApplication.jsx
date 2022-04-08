eb5c4918734e0a0330fabd74867f59cd38d69dd0
AlexKarb <alex.workinfo97@gmail.com>
2022-04-08 15:58:23 +0300

    s

--- src/module/ApplicationList/component/type/ActiveApplication/ActiveApplication.jsx
+++ src/module/ApplicationList/component/type/ActiveApplication/ActiveApplication.jsx
@@ -1,6 +1,3 @@
-import { useEffect, useState } from 'react';
-import { getPendingPublications } from 'service/api-service';
-// import { exempleOfResponse } from 'service/del---exemple_of_res';
 import { ApplicationList } from '../../ApplicationList/ApplicationList';
 
 export const ActiveApplication = () => {
